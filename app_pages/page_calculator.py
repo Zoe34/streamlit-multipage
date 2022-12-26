e4b6860041c4d5d048aa5d978bb7b345b6fd16f2
Zofia <z.przybyl11@gmail.com>
2022-12-26 17:10:32 +0000

    Create a calculator body

--- app_pages/page_calculator.py
+++ app_pages/page_calculator.py
@@ -1,2 +1,36 @@
+import streamlit as st
 
-    
+
+
+def calculator_body():
+
+    #divides app logic from the data
+    st.write('---')
+
+    col1, col2, col3 = st.beta_columns(3)
+    with col1:
+        num1 = st.number_input(label = 'enter the first integer', step = 1)
+
+    with col2:
+        num2 = st.number_input(label = 'enter the second integer', step = 2)
+
+    with col3:
+        operator = st.select_box(label = 'select an operator', options= ['Add', 'Subtract', 'Divide', 'Multiply'])
+
+    # add the logic
+
+    if st.button('Click here for result'):
+        if num2 == 0 and operator == 'Divide':
+            st.error('Cannot divide by zero')
+        else:
+             calculator_function(num1, num2, operator)
+
+
+
+def calculator_function(num1, num2, operator):
+    if operator == 'Add': result = num1 + num2
+    elif operator == 'Subtract': result = num1 - num2
+    elif operator == 'Divide': result = num1/num2
+    elif operator == 'Multiply': result = num1 * num2
+
+
